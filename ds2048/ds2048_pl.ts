de5bb8b84fea4fb41660eb80012f3f36934924c7
pavbaranov <pavbaranov@gmail.com>
2020-01-02 00:26:03 +0100

    Add ds2048 with polish translation

---
+++ ds2048/ds2048_pl.ts
@@ -0,0 +1,111 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Okno główne</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="455"/>
+        <source>Score
+0</source>
+        <translation>Wynik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="900"/>
+        <source>Record
+0</source>
+        <translation>Rekord</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="917"/>
+        <source>22.02.2017</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="930"/>
+        <source>&lt;html>&lt;head/>&lt;body>&lt;p>&lt;a href="http://dansoft.krasnokamensk.ru">&lt;span style=" text-decoration: underline; color:#0000ff;">http://dansoft.krasnokamensk.ru&lt;/span>&lt;/a>&lt;/p>&lt;/body>&lt;/html></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="954"/>
+        <source>2048</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="972"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="979"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="989"/>
+        <source>&amp;New game</source>
+        <translation>&amp;Nowa gra</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="994"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Zakończ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="999"/>
+        <source>Color</source>
+        <translation>Kolor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1004"/>
+        <location filename="mainwindow.cpp" line="69"/>
+        <source>About</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1009"/>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>How to play</source>
+        <translation>Jak grać</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <source>Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!</source>
+        <translation>Użyj klawiszy strzałek do przeniesienia kafli. Dwa kafle tej samej wartości połączą się.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>You won !</source>
+        <translation>Wygrałeś!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>Victory</source>
+        <translation>Zwycięstwa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>Loser</source>
+        <translation>Przegrane</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>You lost !</source>
+        <translation>Przegrałeś!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <source>Score</source>
+        <translation>Wynik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="115"/>
+        <source>Record</source>
+        <translation>Rekord</translation>
+    </message>
+</context>
+</TS>
